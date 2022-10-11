385114be0bb88f136f541063dc8c5cb8e6dca7aa
Gabriel <gamcode98@gmail.com>
2022-10-10 22:20:45 -0300

    working

--- src/route/Navigation/Navigation.jsx
+++ src/route/Navigation/Navigation.jsx
@@ -1,16 +1,18 @@
-import { useContext, useEffect } from 'react';
-import { Link, Outlet } from 'react-router-dom';
-import { UserContext } from '../../context/UserContext';
-import './Navigation.css';
-import logo from "../../assets/logo.png"
+import { useContext, useEffect } from "react";
+import { Link, Outlet } from "react-router-dom";
+import { UserContext } from "../../context/UserContext";
+import "./Navigation.css";
+import logo from "../../assets/logo.png";
 const Navigation = () => {
   const { currentUser, setCurrentUser } = useContext(UserContext);
 
   useEffect(() => {
-    const userStored = localStorage.getItem('currentUser');
-    console.log({ userStored });
-    if (userStored) {
-      setCurrentUser(JSON.parse(userStored));
+    const data = localStorage.getItem("data");
+
+    // console.log({ userStored });
+    if (data) {
+      const dataStored = JSON.parse(data);
+      setCurrentUser(dataStored.user);
     }
   }, []);
 
@@ -25,9 +27,9 @@ const Navigation = () => {
           <img src={logo} alt="Logo" className="logo" />
         </Link>
         <div className="nav-links-container">
-        <Link className="nav-link" to="/">
-              Home
-            </Link>
+          <Link className="nav-link" to="/">
+            Home
+          </Link>
           {currentUser ? (
             <Link className="nav-link" to="/weather/create">
               New Weather
